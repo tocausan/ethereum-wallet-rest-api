5842c628363218f526dadb072843d7fcbdcfebfc
tocausan <tocausan@gmail.com>
2018-04-07 00:06:17 +0200

    cors middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,13 @@
-import { Router } from 'express';
+import {Router} from 'express';
 
-export default ({ config, db }) => {
-	let routes = Router();
+export default ({config, db}) => {
+    let routes = Router()
+        .use((req, res, next) => {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
 
-	// add middleware here
-
-	return routes;
+    return routes;
 }
